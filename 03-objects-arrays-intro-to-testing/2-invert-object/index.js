cb6f4d505c9f9afe3abfaabc6e1b0671f6486f53
mdiabliuk <m.diabliuk@astoundcommerce.com>
2021-03-22 16:37:57 +0200

    03-invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    const newObj = {};
 
+    if (obj) {
+        for (const key in obj) {
+            newObj[obj[key]] = key
+        }
+    
+        return newObj;
+    } else {
+        return undefined;
+    }
 }
