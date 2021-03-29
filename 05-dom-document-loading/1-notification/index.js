df5f80c3fa4cd0cfb1d3c269a71fac0028ce7dce
mdiabliuk <m.diabliuk@astoundcommerce.com>
2021-03-29 08:13:04 +0300

    05-dom-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,36 @@
 export default class NotificationMessage {
+    constructor ({
+       message = ''
+    },{
+        duration = 1000,
+        type = 'success' 
+    } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
 
+        this.render();
+    }
+
+    get createTemplate() {
+        return `
+        <div class="notification success" style="--value: ${this.duration}">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    render () {
+        const element = document.createElement('div');
+
+        element.innerHTML = this.createTemplate;
+
+        this.element = element.firstElementChild;
+    }
 }
