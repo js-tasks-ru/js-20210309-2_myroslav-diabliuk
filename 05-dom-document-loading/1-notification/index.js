9e61056ffb73ee949368ecdb9a75d0199b3cb27a
mdiabliuk <m.diabliuk@astoundcommerce.com>
2021-03-30 22:36:28 +0300

    05-dom - Notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,20 +1,15 @@
 export default class NotificationMessage {
-    constructor ({
-       message = ''
-    },{
-        duration = 1000,
+    constructor (message = 'default message', {
+        duration = 10,
         type = 'success' 
     } = {}) {
         this.message = message;
         this.duration = duration;
         this.type = type;
-
-        this.render();
     }
 
     get createTemplate() {
         return `
-        <div class="notification success" style="--value: ${this.duration}">
             <div class="timer"></div>
             <div class="inner-wrapper">
                 <div class="notification-header">${this.type}</div>
@@ -22,15 +17,43 @@ export default class NotificationMessage {
                     ${this.message}
                 </div>
             </div>
-        </div>
         `
     }
 
-    render () {
+    messageCreate () {
+        const wrapper = document.querySelector('.wrapper');
         const element = document.createElement('div');
 
+        element.setAttribute('class',`notification ${this.type}`);
+        element.setAttribute('style',`--value: ${this.duration}ms`)
+        
         element.innerHTML = this.createTemplate;
 
-        this.element = element.firstElementChild;
+        return wrapper.append(element);
+    }
+
+    show() {
+        const button = document.getElementById('btn1');
+        let activeMsg = button.getAttribute('data-active-message');
+        
+        if (activeMsg === 'false') {
+            this.messageCreate();
+
+            button.setAttribute('data-active-message','true');
+
+            button.disabled = true;
+
+            setTimeout(this.destroy, this.duration);
+        }
+    }
+
+    destroy() {
+        const msg = document.querySelector('.notification');
+        const button = document.getElementById('btn1');
+
+        msg.remove();
+
+        button.setAttribute('data-active-message','false');
+        button.disabled = false;
     }
 }
